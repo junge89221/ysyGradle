3b65a66f9c161a80df8fe73bd60810be9957c891
zhangjun <itjunge@163.com>
2018-03-29 15:55:28 +0800

    Initial commit

---
+++ app/src/main/java/pay/braing/cn/ysygradle/MainActivity.java
@@ -0,0 +1,13 @@
+package pay.braing.cn.ysygradle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
